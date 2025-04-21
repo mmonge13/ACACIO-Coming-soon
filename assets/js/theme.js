d141fe04141db16f399c362b037e1eb1d4780094
Mariano Monge <mariano10monge@gmail.com>
2025-04-21 13:07:51 -0300

    Ajustes de coloresdinamicos y cambios en estilos para modo oscuro

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,15 +3,18 @@
 // ================================================
 const toggleBtn = document.getElementById("toggle-dark-mode");
 const icon = toggleBtn?.querySelector(".icon");
+const logo = document.getElementById("logo");
 
 // Función para aplicar el tema
 function applyTheme(theme) {
   if (theme === "dark") {
     document.body.classList.add("dark-mode");
     if (icon) icon.src = "/assets/images/icons8-light-mode-linear-outline-icons-32.png";
+    if (logo) logo.src = "/assets/images/logoacacio-dark.webp";
   } else {
     document.body.classList.remove("dark-mode");
     if (icon) icon.src = "/assets/images/icons8-night-mode-linear-outline-icons-32.png";
+    if (logo) logo.src = "/assets/images/logoacacio-light.webp"; // Cambia al logo light
   }
 }
 
